c5a5fa5bc929e80c419f0321751a4fc8de74de03
kkx7787 <h016@alpacoedu.onmicrosoft.com>
2024-10-23 09:54:50 +0900

    refactor: 공통 컴포넌트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Layout from './components/template/Layout.tsx';
 import LoanRecommend from './pages/LoanRecommned.tsx';
 import Main from './pages/Main.tsx';
 import MyPage from './pages/MyPage.tsx';
@@ -11,15 +12,17 @@ import RealEstateList from './pages/RealEstateList.tsx';
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/my-page" element={<MyPage />} />
-        <Route path="/loan-recommend" element={<LoanRecommend />} />
-        <Route path="/real-estate-list" element={<RealEstateList />} />
-        <Route path="/real-estate-detail" element={<RealEstateDetail />} />
-        <Route path="/property-from" element={<PropertyForm />} />
-        <Route path="/property-agree" element={<PropertyAgree />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/my-page" element={<MyPage />} />
+          <Route path="/loan-recommend" element={<LoanRecommend />} />
+          <Route path="/real-estate-list" element={<RealEstateList />} />
+          <Route path="/real-estate-detail" element={<RealEstateDetail />} />
+          <Route path="/property-from" element={<PropertyForm />} />
+          <Route path="/property-agree" element={<PropertyAgree />} />
+        </Routes>
+      </Layout>
     </div>
   );
 }
--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -1,3 +1,15 @@
-export default function Layout() {
-  return <div></div>;
+import React from 'react';
+
+interface LayoutProps {
+  children: React.ReactNode;
 }
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div>
+      <main className="flex justify-center">{children}</main>
+    </div>
+  );
+};
+
+export default Layout;

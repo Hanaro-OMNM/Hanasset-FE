956bef70a0120528c372007ba71f186636d6f6fc
mkngkm <mikang1031@gmail.com>
2024-10-23 13:29:55 +0900

    [PROJ-29] feat: Input Atomic 금액, error메세지 추가

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -16,40 +16,76 @@ interface InputProps {
   description?: string;
   className?: string; // 추가된 prop
   inputClassName?: string; // Input 자체에 대한 추가 클래스 prop
-  placeholder?: string; // placeholder prop 추가
+  placeholder?: string; // 일반 placeholder prop 추가
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void; // 추가된 onChange prop
+  error?: boolean; // 에러 상태 prop 추가
+  errorMessage?: string; // 에러 메시지 prop 추가
+  isAmount?: boolean; // 금액 여부 판단 prop 추가
 }
 
 const Input: React.FC<InputProps> = (props) => {
   const [value, setValue] = useState<string>('');
+  const [isFocused, setIsFocused] = useState<boolean>(false);
 
   useEffect(() => {
     setValue(props.value || '');
   }, [props.value]);
 
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = () => setIsFocused(false);
+
   return (
-    <div className={clsx('w-full max-w-md px-2')}>
+    <div className={clsx('w-full max-w-md px-2', props.className)}>
       {props.label && (
-        <label className="text-sm font-medium text-white">{props.label}</label>
+        <label className="text-xs font-medium text-gray-500">
+          {props.label}
+        </label>
       )}
       {props.description && (
-        <p className="text-sm text-white/50">{props.description}</p>
+        <p className="text-sm text-gray-100">{props.description}</p>
       )}
-      <input
-        name={props.name}
-        type={props.type || 'text'}
-        required={props.required}
-        pattern={props.pattern}
-        maxLength={props.maxLength}
-        title={props.title}
-        readOnly={props.readOnly}
-        checked={props.checked}
-        onClick={props.onClick}
-        value={value}
-        onChange={props.onChange}
-        placeholder={props.placeholder}
-        className={clsx(props.inputClassName)}
-      />
+      <div className="relative mb-4">
+        <input
+          name={props.name}
+          type={props.type || 'text'}
+          required={props.required}
+          pattern={props.pattern}
+          maxLength={props.maxLength}
+          title={props.title}
+          readOnly={props.readOnly}
+          checked={props.checked}
+          onClick={props.onClick}
+          value={value}
+          onChange={props.onChange}
+          placeholder={isFocused || value ? '' : '0'}
+          className={clsx(
+            'flex-grow border rounded p-2 transition duration-200 outline-none pr-10 text-left',
+            {
+              'border-gray-300': !isFocused && !props.error,
+              'border-blue-500': isFocused,
+              'border-red-500 bg-red-50': props.error,
+            },
+            props.inputClassName
+          )}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+        />
+        {props.isAmount && (
+          <span
+            className={clsx('absolute right-2 top-2', {
+              'text-red-500': props.error, // 에러 상태일 때 빨간색
+              'text-gray-500': !props.error, // 에러가 아닐 때 기본 색상
+            })}
+          >
+            만원
+          </span>
+        )}
+      </div>
+      {props.error && (
+        <p className="text-red-500 text-xs">
+          {props.errorMessage || '입력란을 올바르게 작성해주세요.'}
+        </p>
+      )}
     </div>
   );
 };

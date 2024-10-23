b2c5afb80c6d7f6282144b3e582194b78fbbc1e0
kkx7787 <h016@alpacoedu.onmicrosoft.com>
2024-10-23 09:55:54 +0900

    style: 컨벤션 미적용된 파일 Prettier 적용

--- src/components/atoms/Dropdown.tsx
+++ src/components/atoms/Dropdown.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
 import { Combobox } from '@headlessui/react';
 import { AiOutlineCheck } from 'react-icons/ai';
 import { FiChevronDown } from 'react-icons/fi';
+import { useState } from 'react';
 
 interface ComboboxProps {
   items: string[]; // 드롭다운 항목들
@@ -12,27 +12,26 @@ interface ComboboxProps {
 }
 
 const DropdownCombobox: React.FC<ComboboxProps> = ({
-                                                     items,
-                                                     selectedItem,
-                                                     setSelectedItem,
-                                                     comboboxClassName = '',
-                                                     optionClassName = '',
-                                                   }) => {
+  items,
+  selectedItem,
+  setSelectedItem,
+  comboboxClassName = '',
+  optionClassName = '',
+}) => {
   const [query, setQuery] = useState('');
 
   const filteredItems =
     query === ''
       ? items
       : items.filter((item) =>
-        item.toLowerCase().includes(query.toLowerCase()),
-      );
+          item.toLowerCase().includes(query.toLowerCase())
+        );
 
   return (
     <Combobox value={selectedItem} onChange={setSelectedItem}>
       <div className={`relative w-64 ${comboboxClassName}`}>
         {/* Combobox Input */}
-        <div
-          className="relative w-full flex items-center space-x-2 border border-[#008485] rounded-md px-3 py-2 bg-white">
+        <div className="relative w-full flex items-center space-x-2 border border-[#008485] rounded-md px-3 py-2 bg-white">
           <Combobox.Input
             className="w-full border-none focus:ring-0 text-black"
             placeholder="Select an option"
@@ -40,14 +39,16 @@ const DropdownCombobox: React.FC<ComboboxProps> = ({
             displayValue={(item: string) => item}
           />
           <Combobox.Button>
-            <FiChevronDown className="w-5 h-5 text-[#008485]" aria-hidden="true" />
+            <FiChevronDown
+              className="w-5 h-5 text-[#008485]"
+              aria-hidden="true"
+            />
           </Combobox.Button>
         </div>
 
         {/* Combobox Options */}
         {filteredItems.length > 0 && (
-          <Combobox.Options
-            className="absolute w-full mt-2 max-h-60 overflow-auto bg-white border border-gray-300 rounded-md shadow-lg z-10">
+          <Combobox.Options className="absolute w-full mt-2 max-h-60 overflow-auto bg-white border border-gray-300 rounded-md shadow-lg z-10">
             {filteredItems.map((item, index) => (
               <Combobox.Option
                 key={index}
@@ -69,7 +70,10 @@ const DropdownCombobox: React.FC<ComboboxProps> = ({
                     </span>
                     {selected && (
                       <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-white">
-                        <AiOutlineCheck className="w-5 h-5" aria-hidden="true" />
+                        <AiOutlineCheck
+                          className="w-5 h-5"
+                          aria-hidden="true"
+                        />
                       </span>
                     )}
                   </>

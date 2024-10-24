b272c870a4723b4624b157fba3e3833788ae09b9
leedy903 <leedy903@naver.com>
2024-10-24 09:14:02 +0900

    [proj-33] feat: 대출 추천 레이아웃 구현

--- src/pages/LoanRecommned.tsx
+++ src/pages/LoanRecommned.tsx
@@ -1,119 +1,317 @@
+// LoanInfoPage.tsx
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react';
+import clsx from 'clsx';
+import React, { useState, useEffect } from 'react';
+
+// Dummy Datas
 interface Loan {
-  fin_inst: string;
-  loan_name: string;
+  logoSrcUrl: string;
+  finInst: string;
+  name: string;
   rate: string;
   amount: string;
 }
 
-const hanaloan: Loan = {
-  fin_inst: '하나은행',
-  loan_name: '하나신용대출',
-  rate: '8.4',
+const hanaBankLoan: Loan = {
+  logoSrcUrl: 'https://www.hanafn.com/assets/img/ko/info/img-hana-symbol.png',
+  finInst: '하나은행',
+  name: '하나신용대출',
+  rate: '8.4%',
   amount: '1,300만원',
 };
 
-const kbloan: Loan = {
-  fin_inst: 'KB국민카드',
-  loan_name: 'KB국민이지플러스',
+const kbCardLoan: Loan = {
+  logoSrcUrl: 'https://www.kbfg.com/kor/images/about/pc/img_symbol_logo.jpg',
+  finInst: 'KB국민카드',
+  name: 'KB국민이지플러스',
   rate: '14.3%',
   amount: '2,200만원',
 };
 
-const loanList: Loan[] = [hanaloan, kbloan];
+const kbBankLoan: Loan = {
+  logoSrcUrl: 'https://www.kbfg.com/kor/images/about/pc/img_symbol_logo.jpg',
+  finInst: 'KB국민은행',
+  name: 'KB비상금대출',
+  rate: '12.5%',
+  amount: '2,000만원',
+};
 
-export default function LoanRecommend() {
+const loanList: Loan[] = [hanaBankLoan, kbCardLoan, kbBankLoan];
+
+// OrderButton Components
+interface OrderButtonProps {
+  activate: boolean;
+  onClick: () => void;
+  text: string;
+}
+
+const OrderButton: React.FC<OrderButtonProps> = ({
+  activate,
+  onClick,
+  text,
+}) => {
   return (
-    <div className="w-[375px] h-[676px] relative bg-[#f4f6f9]">
-      <div className="w-[375px] h-[62.08px] left-0 top-0 absolute">
-        <div className="w-[375px] h-[62.08px] left-0 top-0 absolute bg-neutral-50" />
-        {/* header */}
-        <div className="left-[58px] top-[19px] absolute text-center text-[#5a5657] text-base font-semibold font-['Poppins'] leading-normal">
-          맞춤 상품 안내
+    <div>
+      <button
+        className={clsx(
+          activate && 'text-xs font-semibold',
+          !activate && 'text-xs text-hanaSilver80 font-semibold'
+        )}
+        onClick={onClick}
+      >
+        {text}
+      </button>
+    </div>
+  );
+};
+
+// LogoImage Components
+interface LogoImageProps {
+  logoSrcUrl: string;
+}
+
+const LogoImage: React.FC<LogoImageProps> = ({ logoSrcUrl }) => {
+  return (
+    <div className="w-12 h-12 rounded-[10%]">
+      <img
+        className="h-full w-full rounded-md bg-hanaBlack20 object-cover"
+        src={logoSrcUrl}
+        alt="fin_inst_logo"
+      />
+    </div>
+  );
+};
+
+// FinInst Components
+interface FinInstProps {
+  isHana: boolean;
+  finInst: string;
+}
+
+const FinInst: React.FC<FinInstProps> = ({ isHana, finInst }) => {
+  return (
+    <h5
+      className={clsx(
+        isHana && 'mt-1 h-5 text-hanaBlack20 text-sm font-semibold mb-auto',
+        !isHana && 'mt-1 h-5 text-hanaBlack80 text-sm font-semibold mb-auto'
+      )}
+    >
+      {finInst}
+    </h5>
+  );
+};
+
+// LoanRate Components
+interface LoanRateProps {
+  isHana: boolean;
+  rate: string;
+}
+
+const LoanRate: React.FC<LoanRateProps> = ({ isHana, rate }) => {
+  return (
+    <h6
+      className={clsx(
+        isHana && 'h-5 text-hanaGold20 text-xs font-semibold mt-auto',
+        !isHana && 'h-5 text-black text-xs font-semibold mt-auto'
+      )}
+    >
+      {rate}
+    </h6>
+  );
+};
+
+// LoanName Components
+interface LoanNameProps {
+  isHana: boolean;
+  name: string;
+}
+
+const LoanName: React.FC<LoanNameProps> = ({ isHana, name }) => {
+  return (
+    <h6
+      className={clsx(
+        isHana && 'mt-1 h-5 text-hanaSilver40 text-xs font-medium align-middle',
+        !isHana && 'mt-1 h-5 text-hanaSilver text-xs font-medium align-middle'
+      )}
+    >
+      {name}
+    </h6>
+  );
+};
+
+// LoanAmount Components
+interface LoanAmountProps {
+  isHana: boolean;
+  amount: string;
+}
+
+const LoanAmount: React.FC<LoanAmountProps> = ({ isHana, amount }) => {
+  return (
+    <h6
+      className={clsx(
+        isHana && 'h-5 text-hanaGold20 text-xs font-semibold',
+        !isHana && 'h-5 text-black text-xs font-semibold'
+      )}
+    >
+      {amount}
+    </h6>
+  );
+};
+
+// LoanCard Components
+interface LoanCardProps {
+  isHana: boolean;
+  logoSrcUrl: string;
+  finInst: string;
+  name: string;
+  rate: string;
+  amount: string;
+}
+
+const LoanCard: React.FC<LoanCardProps> = ({
+  isHana,
+  logoSrcUrl,
+  finInst,
+  name,
+  rate,
+  amount,
+}) => {
+  return (
+    <div>
+      <div
+        className={clsx(
+          isHana && 'bg-hanaGreen80 p-4 rounded-lg shadow flex gap-2',
+          !isHana && 'bg-hanaSilver20 p-4 rounded-lg shadow flex gap-2'
+        )}
+      >
+        <LogoImage logoSrcUrl={logoSrcUrl} />
+
+        <div className="w-24 flex-col justify-center items-center">
+          <FinInst isHana={isHana} finInst={finInst} />
+          <LoanRate isHana={isHana} rate={rate} />
         </div>
-      </div>
 
-      <div className="left-[208px] top-[76px] absolute text-[#5a5657] text-base font-semibold font-['Inter']">
-        내가 가진 자산이에요
-      </div>
-
-      <div className="left-[295px] top-[102px] absolute text-[#898989] text-sm font-semibold font-['Inter']">
-        100억!
-      </div>
-
-      <div className="left-[31px] top-[236px] absolute text-[#5a5657] text-base font-semibold font-['Inter']">
-        나에게 맞는 대출 상품을 찾았어요
-      </div>
-
-      <div className="w-[326px] h-[148px] left-[24px] top-[457px] absolute">
-        <div className="w-[326px] h-[69px] left-0 top-[79px] absolute">
-          {/* loan card */}
-          {loanList.map((loan: Loan) => (
-            <>
-              <div className="w-[326px] h-[69px] left-0 top-0 absolute bg-neutral-50 rounded-[10px] shadow" />
-              <div className="w-[45.14px] h-[45px] left-[12.04px] top-[12px] absolute bg-[#fff5d9] rounded-xl" />
-              <div className="w-[71.22px] h-9 left-[69.21px] top-[17px] absolute">
-                <div className="w-[71.22px] left-0 top-0 absolute text-[#5a5657] text-sm font-semibold font-['Inter']">
-                  {loan.fin_inst}
-                </div>
-                <div className="w-[35.11px] left-0 top-[21px] absolute text-black text-xs font-semibold font-['Inter']">
-                  {loan.rate}
-                </div>
-              </div>
-              <div className="w-[108.33px] h-9 left-[166.51px] top-[17px] absolute">
-                <div className="w-[108.33px] left-0 top-0 absolute text-[#8d8c88] text-xs font-medium font-['Inter']">
-                  {loan.loan_name}
-                </div>
-                <div className="w-[57.18px] left-0 top-[21px] absolute text-black text-xs font-semibold font-['Inter']">
-                  {loan.amount}
-                </div>
-              </div>
-            </>
-          ))}
-        </div>
-      </div>
-
-      <div className="w-[155px] h-11 left-[24px] top-[279px] absolute">
-        <div className="w-[155px] h-11 left-0 top-0 absolute bg-[#008485] rounded-[15px]" />
-        <div className="left-[45.54px] top-[10px] absolute text-center text-neutral-50 text-base font-semibold font-['Poppins'] leading-normal">
-          전세 대출
-        </div>
-      </div>
-
-      <div className="w-[155px] h-11 left-[195px] top-[279px] absolute">
-        <div className="w-[155px] h-11 left-0 top-0 absolute bg-[#eeeeec] rounded-[15px]" />
-        <div className="left-[45.54px] top-[10px] absolute text-center text-[#5a5657] text-base font-semibold font-['Poppins'] leading-normal">
-          신용 대출
-        </div>
-      </div>
-
-      <div className="w-[326px] h-[69px] left-[24px] top-[376px] absolute" />
-
-      <div className="left-[36px] top-[342px] absolute text-black text-xs font-semibold font-['Inter']">
-        금리 낮은 순
-      </div>
-
-      <div className="left-[106px] top-[342px] absolute text-[#979797] text-xs font-semibold font-['Inter']">
-        한도 높은 순
-      </div>
-
-      <div className="w-[69px] h-[44.19px] left-[178px] top-[128px] absolute">
-        <img
-          className="w-[69px] h-[44.19px] left-0 top-0 absolute"
-          src="https://via.placeholder.com/69x44"
-        />
-        <div className="w-[62px] h-[34px] left-[7px] top-[3px] absolute text-[#faf9f7] text-[11px] font-bold font-['Nunito Sans']">
-          자본금 10억 대출금 5억
-        </div>
-      </div>
-
-      {/* slider */}
-      <div className="w-[321px] h-[25px] left-[27px] top-[170px] absolute">
-        <div className="w-[353px] h-[7px] p-2 left-[-31px] top-[8px] absolute bg-[#b5b5b5] rounded-[39px]" />
-        <div className="left-0 top-[2px] absolute justify-start items-center inline-flex">
-          <div className="w-[186px] h-2 bg-[#008485] rounded-[39px]" />
-          <div className="w-5 h-5 bg-[#008485] rounded-full" />
+        <div className="flex-col justify-center">
+          <LoanName isHana={isHana} name={name} />
+          <LoanAmount isHana={isHana} amount={amount} />
         </div>
       </div>
     </div>
   );
-}
+};
+
+const LoanInfoPage: React.FC = () => {
+  const [assets, setAssets] = useState(100); // 자산 금액을 위한 state
+  const [activate, setActivate] = useState(true);
+
+  const onClick = () => {
+    console.log('정렬해요.', activate);
+    setActivate((activate) => !activate);
+  };
+
+  useEffect(() => {
+    console.log('우선 순위 변경 로직 동작이 필요해요');
+  }, [activate]);
+
+  return (
+    <div className="min-h-screen bg-[#f4f6f9]">
+      {/* 헤더 */}
+      <div className="flex items-center justify-between">
+        <button className="text-gray-600">뒤로가기</button>
+        <h1 className="text-lg font-semibold">맞춤 상품 안내</h1>
+      </div>
+
+      {/* 자산 정보 */}
+      <div className="mt-6">
+        <h2 className="text-center text-gray-700">내가 가진 자산이에요</h2>
+        <div className="text-center text-2xl font-bold mt-2">{assets}억</div>
+        <div className="flex justify-center mt-4">
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={assets}
+            onChange={(e) => setAssets(Number(e.target.value))}
+            className="w-3/4"
+          />
+        </div>
+      </div>
+
+      <h3 className="ml-4 mt-8 mb-6 text-hanaBlack80 font-semibold">
+        나에게 맞는 대출 상품을 찾았어요
+      </h3>
+
+      {/* 대출 종류 선택 */}
+
+      <TabGroup>
+        <TabList className={'flex gap-x-3'}>
+          <Tab className="ml-4 w-40 h-12 bg-hanaSilver40 text-hanaBlack80 font-semibold rounded-2xl">
+            {({ selected }) => (
+              <button
+                className={clsx(
+                  selected &&
+                    'w-40 h-12 bg-hanaGreen text-hanaSilver20 text-center rounded-2xl'
+                )}
+              >
+                전세 대출
+              </button>
+            )}
+          </Tab>
+          <Tab className="mr-4 w-40 h-12 bg-hanaSilver40 text-hanaBlack80 font-semibold rounded-2xl">
+            {({ selected }) => (
+              <button
+                className={clsx(
+                  selected &&
+                    'w-40 h-12 bg-hanaGreen text-hanaSilver20 rounded-2xl'
+                )}
+              >
+                신용 대출
+              </button>
+            )}
+          </Tab>
+        </TabList>
+        <TabPanels>
+          {/* 전세 대출 TabPanel */}
+          <TabPanel>
+            <div>전세 대출 리스트가 들어가야 해요</div>
+          </TabPanel>
+          {/* 신용 대출 TabPanel */}
+          <TabPanel>
+            {/* 대출 상품 리스트 */}
+            <div className="mt-6 mr-4 ml-4">
+              <div className="mt-4 space-y-4">
+                {/* 우선 순위 선택 */}
+                <div className="ml-4 flex gap-2">
+                  <OrderButton
+                    activate={activate}
+                    onClick={onClick}
+                    text="금리 낮은 순"
+                  />
+                  <OrderButton
+                    activate={!activate}
+                    onClick={onClick}
+                    text="한도 높은 순"
+                  />
+                </div>
+
+                {/* 대출 상품 카드 */}
+                {loanList.map((loan: Loan, index: number) => (
+                  <LoanCard
+                    isHana={index === 0 ? true : false}
+                    logoSrcUrl={loan.logoSrcUrl}
+                    finInst={loan.finInst}
+                    rate={loan.rate}
+                    name={loan.name}
+                    amount={loan.amount}
+                  />
+                ))}
+              </div>
+            </div>
+          </TabPanel>
+        </TabPanels>
+      </TabGroup>
+    </div>
+  );
+};
+
+export default LoanInfoPage;

8aeaf9c0508cddbd6ba59ca0e43c517e9fb0ed12
Ji-eun Yang <jidak1165@gmail.com>
2024-10-24 09:20:46 +0900

    [proj-27] fix: SideLayout 잘못된 넓이 수정

--- src/components/template/SideLayout.tsx
+++ src/components/template/SideLayout.tsx
@@ -2,8 +2,8 @@ const SideLayout: React.FC = () => {
   return (
     <div className="w-full h-svh relative bg-[#f8fafb]">
       {/* 사이드 바 Contents */}
-      <div className="w-[590px] h-svh left-[-1px] top-0 absolute">
-        <div className="w-[590px] h-svh left-0 top-0 absolute bg-white/75 backdrop-blur-[10px]" />
+      <div className="w-[480px] h-svh left-[-1px] top-0 absolute">
+        <div className="w-[480px] h-svh left-0 top-0 absolute bg-white/75 backdrop-blur-[10px]" />
       </div>
     </div>
   );

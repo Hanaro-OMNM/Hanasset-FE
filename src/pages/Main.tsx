a7f3a07f8c30c6ab31f8592ef6de2bf369b65189
kkx7787 <h016@alpacoedu.onmicrosoft.com>
2024-10-28 10:29:54 +0900

    [proj-35] feat: 네이버 지도 API 적용 및 구현

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,3 +1,5 @@
+import { FaPlus, FaMinus } from 'react-icons/fa';
+import { FaLocationCrosshairs } from 'react-icons/fa6';
 import {
   Container as MapDiv,
   NaverMap,
@@ -5,8 +7,15 @@ import {
   Overlay,
   useMap,
 } from 'react-naver-maps';
-import { useState, useCallback, CSSProperties, useEffect } from 'react';
+import { useState, useCallback, useRef, useEffect, CSSProperties } from 'react';
 import { accidentDeath } from '../assets/Dummy.tsx';
+import googleMap from '../assets/img/mapCompanyType/구글 지도.png';
+import naverMap from '../assets/img/mapCompanyType/네이버 지도.png';
+import kakaoMap from '../assets/img/mapCompanyType/카카오 지도.png';
+import satellite from '../assets/img/mapType/위성지도.png';
+import general from '../assets/img/mapType/일반지도.png';
+import topographic from '../assets/img/mapType/지형지도.png';
+import Navbar from '../components/template/Navbar';
 
 function MarkerCluster() {
   const navermaps = useNavermaps();
@@ -94,13 +103,32 @@ function MarkerCluster() {
 }
 
 export default function Main() {
+  const naverMaps = useNavermaps();
+  const mapRef = useRef(null);
+
+  const [showControlPanel, setShowControlPanel] = useState(false);
+  const [mapType, setMapType] = useState(naverMaps.MapTypeId.NORMAL);
+
+  const handleZoomIn = () => {
+    if (mapRef.current) {
+      const currentZoom = mapRef.current.getZoom();
+      mapRef.current.setZoom(currentZoom + 1);
+    }
+  };
+
+  const handleZoomOut = () => {
+    if (mapRef.current) {
+      const currentZoom = mapRef.current.getZoom();
+      mapRef.current.setZoom(currentZoom - 1);
+    }
+  };
+
+  const handleChangeMapType = (type: string) => {
+    setMapType(type);
+  };
+
   const navermaps = useNavermaps();
   const [zoom, setZoom] = useState(13);
-  const [draggable, setDraggable] = useState(true);
-  const [disableKineticPan, setDisableKineticPan] = useState(true);
-  const [tileTransition, setTileTransition] = useState(true);
-  const [minZoom, setMinZoom] = useState(7);
-  const [scaleControl, setScaleControl] = useState(true);
   const [center, setCenter] = useState(
     new navermaps.LatLng(37.3595704, 127.105399)
   );
@@ -126,92 +154,226 @@ export default function Main() {
     }
   };
 
-  const normalBtnStyle: CSSProperties = {
-    backgroundColor: '#fff',
-    border: 'solid 1px #333',
-    outline: 'none',
-    borderRadius: '5px',
-    boxShadow: '2px 2px 1px 1px rgba(0, 0, 0, 0.5)',
-    margin: '0 5px 5px 0',
-    padding: '5px 10px',
-  };
-
-  const selectedBtnStyle: CSSProperties = {
-    ...normalBtnStyle,
-    backgroundColor: '#2780E3',
-    color: 'white',
-  };
-
   return (
-    <MapDiv style={{ position: 'relative', width: '100%', height: '700px' }}>
-      <div
+    <div className="w-full h-svh relative bg-[#f8fafb]">
+      <MapDiv
         style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          zIndex: 1000,
-          padding: 5,
+          position: 'relative',
+          width: '100%',
+          height: '100%',
         }}
       >
-        <button style={normalBtnStyle} onClick={handleLocateMe}>
-          현재 위치로 이동
-        </button>
-        <button
-          style={draggable ? selectedBtnStyle : normalBtnStyle}
-          onClick={() => setDraggable((prev) => !prev)}
-        >
-          지도 인터렉션
-        </button>
-        <button
-          style={!disableKineticPan ? selectedBtnStyle : normalBtnStyle}
-          onClick={() => setDisableKineticPan((prev) => !prev)}
-        >
-          관성 드래깅
-        </button>
-        <button
-          style={tileTransition ? selectedBtnStyle : normalBtnStyle}
-          onClick={() => setTileTransition((prev) => !prev)}
-        >
-          타일 fadeIn 효과
-        </button>
-        <button
-          style={scaleControl ? selectedBtnStyle : normalBtnStyle}
-          onClick={() => setScaleControl((prev) => !prev)}
-        >
-          모든 지도 컨트롤
-        </button>
-        <button
-          style={normalBtnStyle}
-          onClick={() => setMinZoom((prev) => (prev === 10 ? 7 : 10))}
-        >
-          최소/최대 줌 레벨: {minZoom} ~ 21
-        </button>
-      </div>
+        <div className="fixed flex right-2 top-1/4">
+          {showControlPanel ? (
+            <div
+              className={`w-96 bg-white shadow-md rounded-lg p-4 transform transition-all duration-300 ease-in-out opacity-100 scale-100`}
+              style={{ marginRight: '10px' }}
+            >
+              <h3 className="text-lg font-semibold text-gray-700 mb-4">
+                지도 유형
+              </h3>
+              <div className="space-y-2">
+                <div className="flex space-x-2">
+                  <button
+                    className={`relative w-full h-full rounded-xl border transition-colors ${
+                      mapType === naverMaps.MapTypeId.NORMAL
+                        ? 'border-2 border-blue-600'
+                        : ''
+                    }`}
+                    onClick={() =>
+                      handleChangeMapType(naverMaps.MapTypeId.NORMAL)
+                    }
+                  >
+                    <img
+                      className="w-full rounded-xl"
+                      src={general}
+                      alt="logo"
+                    />
+                    <div className="absolute inset-0 flex items-center justify-center text-black font-bold text-base">
+                      일반지도
+                    </div>
+                  </button>
+                  <button
+                    className={`relative w-full h-full rounded-xl border transition-colors ${
+                      mapType === naverMaps.MapTypeId.SATELLITE
+                        ? 'border-2 border-blue-600'
+                        : ''
+                    }`}
+                    onClick={() =>
+                      handleChangeMapType(naverMaps.MapTypeId.SATELLITE)
+                    }
+                  >
+                    <img
+                      className="w-full rounded-xl"
+                      src={satellite}
+                      alt="logo"
+                    />
+                    <div className="absolute inset-0 flex items-center justify-center text-white font-bold text-base">
+                      위성지도
+                    </div>
+                  </button>
 
-      <NaverMap
-        zoomControlOptions={{ position: navermaps.Position.TOP_RIGHT }}
-        center={center}
-        defaultZoom={zoom}
-        onZoomChanged={handleZoomChanged}
-        draggable={draggable}
-        pinchZoom={draggable}
-        scrollWheel={draggable}
-        keyboardShortcuts={draggable}
-        disableDoubleTapZoom={!draggable}
-        disableDoubleClickZoom={!draggable}
-        disableTwoFingerTapZoom={!draggable}
-        disableKineticPan={disableKineticPan}
-        tileTransition={tileTransition}
-        minZoom={minZoom}
-        maxZoom={21}
-        scaleControl={scaleControl}
-        logoControl={scaleControl}
-        mapDataControl={scaleControl}
-        mapTypeControl={scaleControl}
-        zoomControl={scaleControl}
-      >
-        <MarkerCluster />
-      </NaverMap>
-    </MapDiv>
+                  <button
+                    className={`relative w-full h-full rounded-xl border transition-colors ${
+                      mapType === naverMaps.MapTypeId.TERRAIN
+                        ? 'border-2 border-blue-600'
+                        : ''
+                    }`}
+                    onClick={() =>
+                      handleChangeMapType(naverMaps.MapTypeId.TERRAIN)
+                    }
+                  >
+                    <img
+                      className="w-full rounded-xl"
+                      src={topographic}
+                      alt="logo"
+                    />
+                    <div className="absolute inset-0 flex items-center justify-center text-black font-bold text-base">
+                      지형지도
+                    </div>
+                  </button>
+                </div>
+              </div>
+
+              <h3 className="text-lg font-semibold text-gray-700 my-4">
+                지도 바로가기
+              </h3>
+              <div className="space-y-2">
+                <div className="flex space-x-10 justify-center">
+                  <div className="flex flex-col items-center">
+                    <button
+                      className={`relative w-12 h-12 rounded-xl border transition-colors`}
+                      onClick={() =>
+                        window.open('https://map.naver.com', '_blank')
+                      }
+                    >
+                      <img
+                        className="w-full h-full rounded-xl"
+                        src={naverMap}
+                        alt="logo"
+                      />
+                    </button>
+                    <span className="text-center mt-2 text-xs">네이버지도</span>
+                  </div>
+
+                  <div className="flex flex-col items-center">
+                    <button
+                      className={`relative w-12 h-12 rounded-xl border transition-colors`}
+                      onClick={() =>
+                        window.open('https://map.kakao.com', '_blank')
+                      }
+                    >
+                      <img
+                        className="w-full h-full rounded-xl"
+                        src={kakaoMap}
+                        alt="logo"
+                      />
+                    </button>
+                    <span className="text-center mt-2 text-xs">카카오맵</span>
+                  </div>
+
+                  <div className="flex flex-col items-center">
+                    <button
+                      className={`relative w-12 h-12 rounded-xl border transition-colors`}
+                      onClick={() =>
+                        window.open('https://www.google.com/maps', '_blank')
+                      }
+                    >
+                      <img
+                        className="w-full h-full rounded-xl"
+                        src={googleMap}
+                        alt="logo"
+                      />
+                    </button>
+                    <span className="text-center mt-2 text-xs">구글맵</span>
+                  </div>
+                </div>
+              </div>
+              <div className="py-8 flex flex-col space-y-2">
+                <hr className="border-t border-gray-300" />
+                <button
+                  className="w-full py-2 bg-white text-black rounded-lg transition-colors"
+                  onClick={() =>
+                    window.open(
+                      'https://data.kbland.kr/publicdata/real-estate-policy',
+                      '_blank'
+                    )
+                  }
+                >
+                  규제지역 안내 바로가기
+                </button>
+                <hr className="border-t border-gray-300" />
+                <button
+                  className="w-full py-2 bg-white text-black rounded-lg transition-colors"
+                  onClick={() =>
+                    window.open(
+                      'https://www.k-pis.go.kr/addMapView.do?ctprvnCode=41&signguCode=41171&emdCode=41171000&pnuCode=41171000&pageNo=1&serKnd=pnsq&homeMet=home',
+                      '_blank'
+                    )
+                  }
+                >
+                  국유재산 바로가기
+                </button>
+                <hr className="border-t border-gray-300" />
+              </div>
+            </div>
+          ) : (
+            <div className="opacity-0 scale-95"></div>
+          )}
+          <button
+            className={`${
+              showControlPanel
+                ? 'bg-[#008485] text-white'
+                : 'bg-white text-[#008485]'
+            } font-semibold px-1 py-1 rounded-md text-xs w-10 h-10 transition-colors flex items-center justify-center`}
+            onClick={() => setShowControlPanel((prev) => !prev)}
+          >
+            지도 도구
+          </button>
+        </div>
+        <div className="fixed right-2 bottom-6 flex flex-col space-y-2">
+          <button
+            className="bg-white text-[#008485] font-semibold px-1 py-1 rounded-md text-xs w-10 h-10 hover:bg-[#008485] hover:text-white flex items-center justify-center"
+            onClick={handleZoomIn}
+          >
+            <FaPlus />
+          </button>
+          <button
+            className="bg-white text-[#008485] font-semibold px-1 py-1 rounded-md text-xs w-10 h-10 hover:bg-[#008485] hover:text-white flex items-center justify-center"
+            onClick={handleZoomOut}
+          >
+            <FaMinus />
+          </button>
+          <button
+            className="flex justify-center items-center w-10 h-10 bg-white text-[#008485] font-semibold rounded-lg hover:bg-[#008485] hover:text-white transition-colors"
+            onClick={handleLocateMe}
+          >
+            <FaLocationCrosshairs />
+          </button>
+        </div>
+        <NaverMap
+          ref={mapRef}
+          mapTypeId={mapType}
+          zoomControl={false}
+          center={center}
+          defaultZoom={zoom}
+          onZoomChanged={handleZoomChanged}
+          draggable={true}
+          pinchZoom={true}
+          scrollWheel={true}
+          keyboardShortcuts={true}
+          disableDoubleTapZoom={false}
+          disableDoubleClickZoom={false}
+          disableTwoFingerTapZoom={false}
+          tileTransition={true}
+          minZoom={5}
+          maxZoom={21}
+          scaleControl={true}
+        >
+          <MarkerCluster />
+        </NaverMap>
+      </MapDiv>
+      <Navbar />
+    </div>
   );
 }

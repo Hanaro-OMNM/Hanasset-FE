24f07d6a871e04c27f1d40cfadaac46320eb12ef
mkngkm <mikang1031@gmail.com>
2024-10-29 13:09:30 +0900

    [PROJ-51] feat: chat profile img add

--- src/pages/chat/ChatMessage.tsx
+++ src/pages/chat/ChatMessage.tsx
@@ -1,5 +1,7 @@
 // ChatMessage.tsx
 import React from 'react';
+import logo from '../../assets/img/logo.png';
+import userProfile from '../../assets/img/profile_ex.jpg';
 
 type ChatMessageProps = {
   sender: 'user' | 'bot';
@@ -28,13 +30,33 @@ const ChatMessage: React.FC<ChatMessageProps> = ({
       )}
 
       <div className={`flex flex-col ${isUser ? 'items-end' : 'items-start'}`}>
-        {/* 상담사 이름 표시 (bot일 때) */}
-        {!isUser && advisorName && (
-          <span className="text-hanaGreen text-xs font-bold mb-1 px-3">
-            {advisorName}
-          </span>
-        )}
+        <div
+          className={`flex mb-1 ${isUser ? 'flex-row-reverse' : 'flex-row'} items-center`}
+        >
+          {/* 상담사 프로필 사진 */}
+          {!isUser && (
+            <img
+              src={logo} // 상담사 프로필 사진 경로
+              alt="Advisor"
+              className="w-6 h-6 rounded-full mr-2" // 크기와 스타일 조정
+            />
+          )}
+          {/* 사용자 프로필 사진 */}
+          {isUser && (
+            <img
+              src={userProfile} // 사용자 프로필 사진 경로
+              alt="User"
+              className="w-6 h-6 rounded-full ml-2" // 크기와 스타일 조정
+            />
+          )}
 
+          {/* 상담사 이름 표시 (bot일 때) */}
+          {!isUser && advisorName && (
+            <span className="text-hanaGreen text-xs font-bold">
+              {advisorName}
+            </span>
+          )}
+        </div>
         <div
           className={`p-3 max-w-64 rounded-lg text-xs ${
             isUser ? 'bg-hanaGreen80 text-white' : 'bg-hanaGreen40'

055b1aa175eb947fa3117ed44f92525061664f76
mkngkm <mikang1031@gmail.com>
2024-10-29 14:11:53 +0900

    [PROJ-51] refactor: ChatMessage 변수 이름 수정

--- src/pages/chat/ChatMessage.tsx
+++ src/pages/chat/ChatMessage.tsx
@@ -15,27 +15,27 @@ const ChatMessage: React.FC<ChatMessageProps> = ({
   responserName,
   responserImage,
 }) => {
-  const issender = subject === 'sender';
+  const isSender = subject === 'sender';
 
   return (
     <div
-      className={`flex ${issender ? 'justify-end' : 'justify-start'}  items-end`}
+      className={`flex ${isSender ? 'justify-end' : 'justify-start'}  items-end`}
     >
       {/* 시간 표시 (sender일 때 왼쪽, 시간이 변했을 때만 표시) */}
-      {issender && lastMessageTime && (
+      {isSender && lastMessageTime && (
         <span className="mr-1 text-[10px] text-gray-400">
           {lastMessageTime}
         </span>
       )}
 
       <div
-        className={`flex flex-col ${issender ? 'items-end' : 'items-start'}`}
+        className={`flex flex-col ${isSender ? 'items-end' : 'items-start'}`}
       >
         <div
-          className={`flex mb-1 ${issender ? 'flex-row-reverse' : 'flex-row'} items-center`}
+          className={`flex mb-1 ${isSender ? 'flex-row-reverse' : 'flex-row'} items-center`}
         >
-          {/* 상담사 프로필 사진 */}
-          {!issender && responserImage && (
+          {/* responser 프로필 사진 */}
+          {!isSender && responserImage && (
             <img
               src={responserImage}
               alt="responser"
@@ -43,8 +43,8 @@ const ChatMessage: React.FC<ChatMessageProps> = ({
             />
           )}
 
-          {/* 상담사 이름 표시 */}
-          {!issender && responserName && (
+          {/* responser 이름 표시 */}
+          {!isSender && responserName && (
             <span className="text-hanaGreen text-xs font-bold">
               {responserName}
             </span>
@@ -52,18 +52,18 @@ const ChatMessage: React.FC<ChatMessageProps> = ({
         </div>
         <div
           className={`p-3 max-w-64 rounded-lg text-xs ${
-            issender ? 'bg-hanaGreen80 text-white' : 'bg-hanaGreen40'
+            isSender ? 'bg-hanaGreen80 text-white' : 'bg-hanaGreen40'
           }`}
           style={{
-            borderRadius: issender ? '15px 15px 0 15px' : '15px 15px 15px 0',
+            borderRadius: isSender ? '15px 15px 0 15px' : '15px 15px 15px 0',
           }}
         >
           <p>{message}</p>
         </div>
       </div>
 
-      {/* 시간 표시 (상담사일 때 오른쪽, 시간이 변했을 때만 표시) */}
-      {!issender && lastMessageTime && (
+      {/* 시간 표시 (responser일 때 오른쪽, 시간이 변했을 때만 표시) */}
+      {!isSender && lastMessageTime && (
         <span className="ml-1 text-[10px] text-gray-400">
           {lastMessageTime}
         </span>

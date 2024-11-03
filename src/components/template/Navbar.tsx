e440d52fbec0a67dfa25988f87254a399967477a
kkx7787 <115688628+kkx7787@users.noreply.github.com>
2024-11-03 17:33:45 +0900

    fix: NavBar 이슈 수정

--- src/components/template/Navbar.tsx
+++ src/components/template/Navbar.tsx
@@ -1,45 +1,25 @@
 import { FaHome, FaMap } from 'react-icons/fa';
 import { FaHouseUser } from 'react-icons/fa6';
 import { MdChat } from 'react-icons/md';
-import { useNavigate } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
 import Logo from '../../assets/img/logo.png';
 import Profile from '../../assets/img/profile_ex.jpg';
-import MainSideLayout from '../template/MainSideLayout';
-import ConsultingTabLayout from './ConsultingTabLayout.tsx';
-import MyPageLayout from './MyPageLayout.tsx';
-import RealEstateLayout from './RealEstateList/RealEstateListLayout.tsx';
 
 interface NavbarProps {
   state?: boolean;
-  onTabChange?: () => void;
   children: React.ReactNode;
 }
 
-export const Navbar: React.FC<NavbarProps> = ({
-  state,
-  onTabChange,
-  children,
-}) => {
-  const [activePage, setActivePage] = useState<
-    'home' | 'map' | 'myPage' | 'consultant' | 'estateList'
-  >('home');
-
+export const Navbar: React.FC<NavbarProps> = ({ state, children }) => {
   const navigate = useNavigate();
-
+  const location = useLocation();
   // 선택된 매물이 있으면 자동으로 'estateList' 페이지로 이동하도록 설정
   useEffect(() => {
     if (state) {
-      setActivePage('estateList');
+      navigate('/real-estate-list');
     }
   }, [state]);
-
-  const handleSetActivePage = (
-    page: 'home' | 'map' | 'myPage' | 'consultant' | 'estateList'
-  ) => {
-    setActivePage(page);
-    onTabChange!(); // 탭이 변경될 때마다 onTabChange 호출하여 Main의 estateState 초기화
-  };
 
   return (
     <div className="space-x-16">
@@ -55,14 +35,14 @@ export const Navbar: React.FC<NavbarProps> = ({
         </a>
         <div className="w-14 h-96 mb-32 flex flex-col items-center justify-between">
           <button
-            onClick={() => navigate('/')}
+            onClick={() => navigate('/home')}
             className="w-14 h-20 flex flex-col items-center"
           >
             <div
-              className={`w-12 h-12 rounded-lg flex items-center justify-center ${activePage === 'home' ? 'bg-hanaGreen shadow mb-2' : 'bg-transparent'}`}
+              className={`w-12 h-12 rounded-lg flex items-center justify-center ${location.pathname === '/home' ? 'bg-hanaGreen shadow mb-2' : 'bg-transparent'}`}
             >
               <FaHome
-                className={`w-6 h-6 ${activePage === 'home' ? 'text-white' : 'text-black'}`}
+                className={`w-6 h-6 ${location.pathname === '/home' ? 'text-white' : 'text-black'}`}
               />
             </div>
             <div className="text-black text-base font-bold font-['Noto Sans KR'] tracking-tight text-center">
@@ -72,14 +52,14 @@ export const Navbar: React.FC<NavbarProps> = ({
 
           {/* 지도 버튼 */}
           <button
-            onClick={() => handleSetActivePage('map')}
+            onClick={() => navigate('/')}
             className="w-14 h-20 flex flex-col items-center"
           >
             <div
-              className={`w-12 h-12 rounded-lg flex items-center justify-center ${activePage === 'map' ? 'bg-hanaGreen shadow mb-2' : 'bg-transparent'}`}
+              className={`w-12 h-12 rounded-lg flex items-center justify-center ${location.pathname === '/' ? 'bg-hanaGreen shadow mb-2' : 'bg-transparent'}`}
             >
               <FaMap
-                className={`w-6 h-6 ${activePage === 'map' ? 'text-white' : 'text-black'}`}
+                className={`w-6 h-6 ${location.pathname === '/' ? 'text-white' : 'text-black'}`}
               />
             </div>
             <div className="text-black text-base font-bold font-['Noto Sans KR'] tracking-tight text-center">
@@ -89,14 +69,14 @@ export const Navbar: React.FC<NavbarProps> = ({
 
           {/* 상담 버튼 */}
           <button
-            onClick={() => handleSetActivePage('consultant')}
+            onClick={() => navigate('/consulting')}
             className="w-14 h-20 flex flex-col items-center"
           >
             <div
-              className={`w-12 h-12 rounded-lg flex items-center justify-center ${activePage === 'consultant' ? 'bg-hanaGreen shadow mb-2' : 'bg-transparent'}`}
+              className={`w-12 h-12 rounded-lg flex items-center justify-center ${location.pathname === '/consulting' ? 'bg-hanaGreen shadow mb-2' : 'bg-transparent'}`}
             >
               <MdChat
-                className={`w-6 h-6 ${activePage === 'consultant' ? 'text-white' : 'text-black'}`}
+                className={`w-6 h-6 ${location.pathname === '/consulting' ? 'text-white' : 'text-black'}`}
               />
             </div>
             <div className="text-black text-base font-bold font-['Noto Sans KR'] tracking-tight text-center">
@@ -106,14 +86,14 @@ export const Navbar: React.FC<NavbarProps> = ({
 
           {/* 마이홈 버튼 */}
           <button
-            onClick={() => handleSetActivePage('myPage')}
+            onClick={() => navigate('/my-page')}
             className="w-14 h-20 flex flex-col items-center"
           >
             <div
-              className={`w-12 h-12 rounded-lg flex items-center justify-center ${activePage === 'myPage' ? 'bg-hanaGreen shadow mb-2' : 'bg-transparent'}`}
+              className={`w-12 h-12 rounded-lg flex items-center justify-center ${location.pathname === '/my-page' ? 'bg-hanaGreen shadow mb-2' : 'bg-transparent'}`}
             >
               <FaHouseUser
-                className={`w-6 h-6 ${activePage === 'myPage' ? 'text-white' : 'text-black'}`}
+                className={`w-6 h-6 ${location.pathname === '/my-page' ? 'text-white' : 'text-black'}`}
               />
             </div>
             <div className="text-black text-base font-bold font-['Noto Sans KR'] tracking-tight text-center">
@@ -128,13 +108,7 @@ export const Navbar: React.FC<NavbarProps> = ({
         />
       </div>
       <div>{children}</div>
-      {/*{activePage === 'home' && <MainSideLayout />}*/}
-      {/*{activePage === 'map' && <></>}*/}
-      {/*{activePage === 'consultant' && <ConsultingTabLayout />}*/}
-      {/*{activePage === 'myPage' && <MyPageLayout />}*/}
-      {/*{activePage === 'estateList' && (*/}
       {/*  <RealEstateLayout onBackClick={() => handleSetActivePage('map')} />*/}
-      {/*)}*/}
     </div>
   );
 };

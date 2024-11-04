29eb2c3e0c1e265bac72f70594b0fb8e1a2f38a0
leedy903 <leedy903@naver.com>
2024-11-04 16:05:34 +0900

    [proj-65] feat: 상담원이 확인할 수 있는 손님의 정보 디자인 수정 (dsr 디자인)

--- src/pages/consultant/GuestDetailInfo.tsx
+++ src/pages/consultant/GuestDetailInfo.tsx
@@ -22,26 +22,16 @@ const GuestDetailInfo: React.FC<GuestDetailInfoProps> = ({
   dsr,
 }) => {
   return (
-    <div className="flex flex-col gap-y-4 px-2 text-hanaBlack80 text-xl font-semibold">
-      <div>
-        <p>기본 정보</p>
-        <div className="flex flex-col gap-y-2 px-4 text-base">
-          <p className="">이름: {name}</p>
-          <p className="">나이: {age}</p>
-          <p className="">직업: {job}</p>
-          <p className="">연소득: {income.toLocaleString()}만원</p>
-          <p className="">결혼자녀유무: {family ? '있음' : '없음'} </p>
-          <p className="">보유 주택: {home ? '있음' : '없음'}</p>
-          <p className="">보유 대출: {loan > 0 ? '있음' : '없음'}</p>
-        </div>
-      </div>
-
-      <div>
-        <p>신용 정보</p>
-        <div className="text-base">
-          <DsrInfo dsr={dsr} />
-        </div>
-      </div>
+    <div className="flex flex-col gap-y-2 px-2 text-hanaBlack80 font-semibold">
+      <p>이름: {name}</p>
+      <p>나이: {age}</p>
+      <p>직업: {job}</p>
+      <p>연소득: {income.toLocaleString()}만 원</p>
+      <p>결혼자녀유무: {family ? '있음' : '없음'} </p>
+      <p>보유 주택: {home ? '있음' : '없음'}</p>
+      <p>보유 대출: {loan > 0 ? '있음' : '없음'}</p>
+      <p>보유 대출: {loan > 0 ? '있음' : '없음'}</p>
+      <p>현재 DSR: {dsr}%</p>
     </div>
   );
 };

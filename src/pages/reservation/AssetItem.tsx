0564b1a02551781b988ee1b65618704684792147
Choeseongeong <tjswjd7166@gmail.com>
2024-11-15 16:27:19 +0900

    [proj-76] refactor: refactor css

--- src/pages/reservation/AssetItem.tsx
+++ src/pages/reservation/AssetItem.tsx
@@ -1,17 +1,18 @@
 // components/atoms/AssetItem.tsx
-import { LuMapPin } from 'react-icons/lu';
+import { PiBuildingApartment } from 'react-icons/pi';
+import CommonBackground from '../../components/atoms/CommonBackground';
 
 interface AssetItemProps {
   name: string;
 }
 
 const AssetItem: React.FC<AssetItemProps> = ({ name }) => (
-  <div className="m-1 p-1 bg-gray-100 rounded-3xl">
+  <CommonBackground className="m-1 p-1">
     <div className="w-full flex h-full items-center p-2">
-      <LuMapPin className="text-xl text-gray-400 ml-2" />
+      <PiBuildingApartment className="text-xl text-hanaColor2 ml-2" />
       <div className="text-black font-medium ml-5">{name}</div>
     </div>
-  </div>
+  </CommonBackground>
 );
 
 export default AssetItem;

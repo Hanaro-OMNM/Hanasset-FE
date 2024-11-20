4e42692fe50f9087ebc80016e36ee162691a5567
mkngkm <mikang1031@gmail.com>
2024-11-20 15:36:27 +0900

    [PROJ-85] fix: 자산입력 중 뒤로가기 시 상담 예약 막기

--- src/pages/reservation/ChatReservation.tsx
+++ src/pages/reservation/ChatReservation.tsx
@@ -1,11 +1,9 @@
 import { useLocation } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
 import { useState } from 'react';
 import reserve_hand from '../../assets/img/reserve_img.png';
 import Button from '../../components/atoms/Button';
 import CommonBackground from '../../components/atoms/CommonBackground';
 import MobileHeader from '../../components/atoms/MobileHeader';
-import loanReservationAtom from '../../recoil/loanReservation/index';
 import { selectedEstateType } from '../../types/global';
 import AssetItem from './AssetItem';
 import DatePicker from './DatePicker';
@@ -69,8 +67,6 @@ export default function ChatReservation() {
 
   const [selectedDate, setSelectedDate] = useState<string>('');
   const [selectedTime, setSelectedTime] = useState<string>('');
-  const [selectedLoanReservation, setSelectedLoanReservation] =
-    useRecoilState(loanReservationAtom);
 
   const handleDateChange = (value: string) => {
     setSelectedDate(value);
@@ -90,15 +86,9 @@ export default function ChatReservation() {
       alert('예약 시간을 선택해주세요.');
       return;
     }
-    if (!selectedLoanReservation.reservationTime) {
-      setSelectedLoanReservation({
-        reservationInfo: estateInfo,
-        reservationTime: selectedDate + ' ' + selectedTime,
-      });
 
-      /* 폼 전환 */
-      setShowForm(true);
-    }
+    /* 폼 전환 */
+    setShowForm(true);
   };
 
   const onBack = (): void => {
@@ -146,7 +136,11 @@ export default function ChatReservation() {
             </div>
           </div>
         ) : (
-          <DynamicFormSwitcher />
+          <DynamicFormSwitcher
+            estateInfo={estateInfo}
+            selectedDate={selectedDate}
+            selectedTime={selectedTime}
+          />
         )}
       </div>
     </div>

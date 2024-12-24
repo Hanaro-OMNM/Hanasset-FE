9b600aa35fabecd3ce4f05cade7c4f8967ac6e3b
leedy903 <leedy903@naver.com>
2024-12-24 15:49:36 +0900

    [proj-115] feat: loan-recommend -> loan으로 주소 변경

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ function App() {
                 <Route path="/home" element={<Main />} />
                 <Route path="/consulting" element={<Consulting />} />
                 <Route path="/my-page" element={<MyPage />} />
-                <Route path="/loan-recommend" element={<LoanRecommend />} />
+                <Route path="/loan" element={<LoanRecommend />} />
                 <Route path="/chat-reservation" element={<ChatReservation />} />
                 <Route path="/real-estate-list" element={<RealEstateList />} />
                 <Route path="/select-estate" element={<SelectEstate />} />

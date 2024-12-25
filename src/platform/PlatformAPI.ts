cece2a5ae7104f04d9946f7d70977c94b702fb83
kkx7787 <kkx7787@naver.com>
2024-12-25 15:08:00 +0900

    [PROJ-124] add: Access Token 만료 시 새로운 토큰 받아오는 로직 추가

--- src/platform/PlatformAPI.ts
+++ src/platform/PlatformAPI.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from 'axios';
+import { jwtDecode } from 'jwt-decode';
 import {
   BirthDate,
   ConfirmCode,
@@ -20,11 +21,51 @@ import {
 } from '../types/hanaAssetResponse.common.ts';
 
 export class PlatformAPI {
-  static instance: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:8080',
-    timeout: 10000,
-    withCredentials: true,
-  });
+  static isTokenExpired = (token: string) => {
+    try {
+      const decoded = jwtDecode(token); // 토큰 디코딩
+      const currentTime = Math.floor(Date.now() / 1000);
+      return decoded.exp! < currentTime; // 만료 여부 확인
+    } catch (error) {
+      console.error('Invalid token', error);
+      return true;
+    }
+  };
+
+  static instance: AxiosInstance = (() => {
+    const instance = axios.create({
+      baseURL: 'http://localhost:8080',
+      timeout: 10000,
+      withCredentials: true,
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    instance.interceptors.response.use(
+      (response) => {
+        const accessToken = localStorage.getItem('accessToken');
+        if (accessToken && this.isTokenExpired(accessToken)) {
+          const authorizationHeader = response.headers.authorization;
+          if (
+            authorizationHeader &&
+            authorizationHeader.startsWith('Bearer ')
+          ) {
+            const accessToken = authorizationHeader.split(' ')[1];
+            localStorage.setItem('accessToken', accessToken);
+            return accessToken;
+          } else {
+            console.error('Authorization header is missing or invalid');
+            return undefined;
+          }
+        }
+        return response;
+      },
+      (error) => {
+        console.error('Response error:', error);
+      }
+    );
+
+    return instance;
+  })();
 
   private static readonly defaultConfig = {
     headers: { 'Content-Type': 'application/json' },

6fc2adc9f9f106dd7586accf0ec3992c0d61df09
leedy903 <leedy903@naver.com>
2024-12-28 03:05:44 +0900

    [proj-135] feat: 돈 단위 수정, 금액에 따라 억 단위, 천만 단위 구분해서 보여주도록 구현

--- src/pages/GuestChatDetail/DetailInformation.tsx
+++ src/pages/GuestChatDetail/DetailInformation.tsx
@@ -12,17 +12,29 @@ const DetailInformtation: React.FC<DetailInformtationProps> = ({
     <div className="flex justify-around mt-4 font-fontLight">
       <div className="flex-col text-hanaBlack80 text-center">
         <div className="text-sm text-hanaSilver80">자본금</div>
-        <div>{Math.round(capital * 10) / 10}천만 원</div>
+        <div>
+          {capital > 10
+            ? Math.round(capital * 10) / 100 + '억 원'
+            : Math.round(capital * 10) / 10 + '천만 원'}
+        </div>
       </div>
       <div className="flex-col text-hanaBlack80 text-center">
         <div className="text-sm text-hanaSilver80">대출금</div>
         <div>
-          {Math.round(Math.min(totalPrice - capital, maxLoan) * 10) / 10}천만 원
+          {Math.min(totalPrice - capital, maxLoan) > 10
+            ? Math.round(Math.min(totalPrice - capital, maxLoan) * 10) / 100 +
+              '억 원'
+            : Math.round(Math.min(totalPrice - capital, maxLoan) * 10) / 10 +
+              '천만 원'}
         </div>
       </div>
       <div className="flex-col text-hanaBlack80 text-center">
         <div className="text-sm text-hanaSilver80">필요 자금</div>
-        <div>{Math.round(totalPrice * 10) / 10}천만 원</div>
+        <div>
+          {totalPrice > 10
+            ? Math.round(totalPrice * 10) / 100 + '억 원'
+            : Math.round(totalPrice * 10) / 10 + '천만 원'}
+        </div>
       </div>
     </div>
   );

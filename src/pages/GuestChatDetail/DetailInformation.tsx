5ee81eaedd5e1efbd7bfe1ed01f722fa3035a215
leedy903 <leedy903@naver.com>
2024-12-28 02:08:57 +0900

    [proj-135] fix: 금액이 최대 소숫점 한 자리까지만 나오도록 변경

--- src/pages/GuestChatDetail/DetailInformation.tsx
+++ src/pages/GuestChatDetail/DetailInformation.tsx
@@ -12,15 +12,17 @@ const DetailInformtation: React.FC<DetailInformtationProps> = ({
     <div className="flex justify-around mt-4 font-fontLight">
       <div className="flex-col text-hanaBlack80 text-center">
         <div className="text-sm text-hanaSilver80">자본금</div>
-        <div>{capital}천만 원</div>
+        <div>{Math.round(capital * 10) / 10}천만 원</div>
       </div>
       <div className="flex-col text-hanaBlack80 text-center">
         <div className="text-sm text-hanaSilver80">대출금</div>
-        <div>{Math.min(Number(totalPrice - capital), maxLoan)}천만 원</div>
+        <div>
+          {Math.round(Math.min(totalPrice - capital, maxLoan) * 10) / 10}천만 원
+        </div>
       </div>
       <div className="flex-col text-hanaBlack80 text-center">
         <div className="text-sm text-hanaSilver80">필요 자금</div>
-        <div>{totalPrice}천만 원</div>
+        <div>{Math.round(totalPrice * 10) / 10}천만 원</div>
       </div>
     </div>
   );
